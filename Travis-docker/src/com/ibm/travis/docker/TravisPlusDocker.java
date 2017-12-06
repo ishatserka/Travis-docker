223c60ee4afc85cab41dfab4cac1404cac742126
Les <Les@Les-PC>
2017-12-06 17:09:47 -0300

    dfhdsfh
     hdsfhs

--- Travis-docker/src/com/ibm/travis/docker/TravisPlusDocker.java
+++ Travis-docker/src/com/ibm/travis/docker/TravisPlusDocker.java
@@ -4,7 +4,9 @@ public class TravisPlusDocker {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("======================\n");
 		System.out.println("Hello Travis!!!");
+		System.out.println("======================\n");
 	}
 
 }
