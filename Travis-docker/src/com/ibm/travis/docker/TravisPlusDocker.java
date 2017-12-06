6b8b4870bdb136be4ecf3f9797591e5d2a21c7d7
Les <Les@Les-PC>
2017-12-06 16:04:29 -0300

    first commit
    hi travis

---
+++ Travis-docker/src/com/ibm/travis/docker/TravisPlusDocker.java
@@ -0,0 +1,10 @@
+package com.ibm.travis.docker;
+
+public class TravisPlusDocker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Travis!!!");
+	}
+
+}
